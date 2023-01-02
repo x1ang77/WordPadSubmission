632b4c6a69400c0eeae2b1ce6c830fdbc238fd35
Xiang Ze <xchuay@gmail.com>
2023-01-02 19:03:13 +0800

    first commit

---
+++ app/src/main/java/com/xiangze/wordpadsubmission/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xiangze.wordpadsubmission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
